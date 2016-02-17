e57df7229c5c064339623f2b0c1d33f1872ac324
Thibaut Moiroud <thibaut.moiroud@kozea.fr>
2016-02-17 15:53:02 +0100

    Simplification/épuration du code et suppression partie login en local.

--- content.sql
+++ content.sql
@@ -1,6 +1,5 @@
-insert into postit (post_id, owner, text, date) values (1, 'thibaut', 'La roue tourne tournera.','2016-01-25');
-insert into postit (post_id, owner, text, date) values (2, 'thibaut', "Tant va la cruche à l'eau qu'à la fin tu me les brise.",'2016-02-08');
-insert into postit (post_id, owner, text, date) values (3, 'toto', '0 + 0 = tête à titi','2016-03-15');
+insert into color (code_color, owner) values ('#ED6C09', 'Guillaume Ayoub');
+insert into color (code_color, owner) values ('#1F9ED5', 'Thibaut Moiroud');
+insert into color (code_color, owner) values ('#FD3F92', 'Clément Plasse');
 
-insert into color (code_color, owner) values ('#ED6C09', 'thibaut');
-insert into color (code_color, owner) values ('#1F9ED5', 'toto');
+insert into postit (post_id, owner, text, date) values (1, 'Clément Plasse', '0 + 0 = tête à titi', '2015-12-05');
--- wallit.py
+++ wallit.py
@@ -1,14 +1,14 @@
-# all the import
 import sqlite3
 import httplib2
+import datetime
 import xml.etree.ElementTree as ET
-from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash
+from flask import (
+    Flask, request, session, g, redirect, url_for, render_template, flash)
 from contextlib import closing
 from functools import wraps
 from oauth2client.client import OAuth2WebServerFlow
 
-# configuration
+
 DATABASE = '/tmp/wallit.db'
 DEBUG = True
 SECRET_KEY = 'development key'
@@ -16,21 +16,22 @@ OAUTH_CLIENT_ID = '197145980271-j21e4i5v6dt3mia217npvkik6t0irj05.apps.googleuser
 OAUTH_SECRET_KEY = 'U9T-UgjX2ngH6ipB9zh9MWHW'
 OAUTH_REDIRECT = 'http://localhost:5000/oauth2callback'
 
-# TODO: change this when we get Google's data
-#USERS = {'thibaut': 'pass', 'toto':'toto'}
 
-#application
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('WALLIT_SETTINGS', silent=True)
 
-FLOW = OAuth2WebServerFlow(client_id=app.config['OAUTH_CLIENT_ID'],
-                           client_secret=app.config['OAUTH_SECRET_KEY'],
-                           scope='https://www.google.com/m8/feeds',
-                           redirect_uri=app.config['OAUTH_REDIRECT'])
+
+FLOW = OAuth2WebServerFlow(
+    client_id=app.config['OAUTH_CLIENT_ID'],
+    client_secret=app.config['OAUTH_SECRET_KEY'],
+    scope='https://www.google.com/m8/feeds',
+    redirect_uri=app.config['OAUTH_REDIRECT'])
+
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
 
 def init_db():
     with closing(connect_db()) as db:
@@ -40,15 +41,18 @@ def init_db():
             db.cursor().executescript(e.read())
         db.commit()
 
+
 @app.before_request
 def before_request():
     g.db = connect_db()
+
 
 @app.teardown_request
 def teardown_request(exception):
     db = getattr(g, 'db', None)
     if db is not None:
         db.close()
+
 
 def auth(function):
     """Wrapper checking if the user is logged in."""
@@ -61,52 +65,27 @@ def auth(function):
         return redirect(authorize_url)
     return wrapper
 
+
 @app.route('/oauth2callback')
 def oauth2callback():
     code = request.args.get('code')
     if code:
-        credentials = FLOW.step2_exchange(code)
-        http = credentials.authorize(httplib2.Http())
-        _, content = http.request('https://www.google.com/m8/feeds/contacts/default/full?v=3.0')
-        data = ET.fromstring(content)
-        session['users'] = [title.text for title in data.findall('./feed/entry')]
-        #régler le problème session['users']
-        session['users'] = ['Guillaume Ayoub']
-        print(session['users'], data, content)
+        # TODO: Fix session['users']
+        # credentials = FLOW.step2_exchange(code)
+        # http = credentials.authorize(httplib2.Http())
+        # _, content = http.request('https://www.google.com/m8/feeds/contacts/default/full?v=3.0')
+        # data = ET.fromstring(content)
+        # session['users'] = [title.text for title in data.findall('./feed/entry')]
+        session['users'] = ['Thibaut Moiroud', 'Guillaume Ayoub', 'Clément Plasse']
         return redirect(url_for('display_wall'))
     else:
         return request.form.get('error')
-"""
-@app.route('/', methods=['GET', 'POST'])
-def display_connection():
-    'Allow the user to connect himself on the application'
-    title = "Login"
-    error = None
-    is_logged = False
-    if request.method == 'POST':
-        the_user_name = request.form['username']
-        the_user_pass = request.form['password']
-        is_logged = (the_user_name, the_user_pass) in app.config['USERS'].items()
-        if is_logged:
-            session['user_id'] = the_user_name
-            flash('You were logged in')
-            return redirect(url_for('display_wall'))
-        else:
-            error = 'Invalid password or login'
-    return render_template('login.html', error=error, title=title)
 
-@app.route('/logout')
-def logout():
-    session.pop('user_id', None)
-    flash('You were logged out')
-    return redirect(url_for('display_connection'))"""
 
 @app.route('/')
 @auth
 def display_wall():
-    """Display all post-its on a wall"""
-    print(session['users'])
-    title = "Home"
+    """Display all post-its on a wall."""
     s = """ select post_id, p.owner, text, date, code_color, x, y
                 from postit p, color c
                 where p.owner = c.owner
@@ -114,32 +93,25 @@ def display_wall():
     cur = g.db.execute(s)
     postits = []
     for row in cur.fetchall():
+        date = datetime.datetime.strptime(row[3], '%Y-%m-%d').strftime('%d/%m/%Y')
         postits.append({
             "post_id": row[0],
             "owner": row[1],
             "text": row[2],
-            "date": row[3],
+            "date": date,
             "color": row[4],
             "x": row[5],
             "y": row[6]
         })
-    for postit in postits:
-        print("DISPLAY_WALL --> ID = ",postit['post_id']," X = ",postit['x'], " Y = ", postit['y'])
-    return render_template('home.html', postits=postits, title=title)
+    return render_template('home.html', postits=postits, title='Home')
 
 @app.route('/save_position', methods=['POST'])
 @auth
 def save_position():
-    """route that get the post request from the page / when we drop
-    a post-it"""
-    pos_x = request.form.get('x')
-    pos_y = request.form.get('y')
-    postit_id = request.form.get('post_id')
-    #print("SAVE_POSITION --> ID = ",postit_id," X = ", pos_x, " Y = ", pos_y)
-    s = """update postit
-    set x="""+pos_x+""", y="""+pos_y+"""
-    where post_id="""+postit_id
-    g.db.execute(s)
+    """Get the post request from the page / when we drop a post-it."""
+    g.db.execute(
+        "update postit set x=?, y=? where post_id=?",
+        [request.form.get(key) for key in ('x', 'y', 'post_id')])
     g.db.commit()
     return redirect(url_for('display_wall'))
 
@@ -149,59 +121,23 @@ def display_config():
     """Allow the user to manage his profile"""
     pass
 
-@app.route('/new', methods=['GET','POST'])
+@app.route('/new', methods=['GET', 'POST'])
 @auth
 def add_post_it():
     """Allow the user to add a new post-it on the wall."""
-    title = "Add post-it"
-    error = None
-    cur = g.db.execute('select owner from color')
-    owners = ([row[0] for row in cur.fetchall()])
-    form_error = {}
-    default_values = {}
     if request.method == 'POST':
-        valid_owner = True
-        valid_text = True
-        valid_date = True
-        if request.form['owner'] in owners:
-            valid_owner = True
-        else:
-            form_error['owner'] = 'No owner?'
-            default_values['text'] = request.form['text']
-            default_values['date'] = request.form['date']
-            valid_owner = False
-        if not request.form['text']:
-            valid_text = False
-            default_values['owner'] = request.form['owner']
-            default_values['date'] = request.form['date']
-            form_error['text'] = 'No message?'
-        else:
-            valid_text = True
-        if not request.form['date']:
-            valid_date = False
-            default_values['owner'] = request.form['owner']
-            default_values['text'] = request.form['text']
-            form_error['date'] = 'No date?'
-        else:
-            valid_date = True
-        if valid_owner and valid_text and valid_date:
-            g.db.execute('insert into postit (owner, text, date) values (?, ?, ?)',
-                [request.form['owner'], request.form['text'],
-                request.form['date']])
-            g.db.commit()
-            flash('A new post-it was successefully added')
-            return redirect(url_for('display_wall'))
-            print('SUCCESS')
-        else:
-            error = ' invalid data'
-    return render_template('new_post_it.html', owners=owners, error=error,
-                form_error=form_error, title=title, defaults=default_values)
+        g.db.execute('insert into postit (owner, text, date) values (?, ?, ?)',
+            [request.form['owner'], request.form['text'],
+            request.form['date']])
+        g.db.commit()
+        flash('A new post-it was successefully added')
+        return redirect(url_for('display_wall'))
+    return render_template('new_post_it.html', title="Add post-it")
 
 @app.route('/statistics')
 @auth
 def display_stats():
     """Display some statistics from the application"""
-    title = "Statistics"
     cur_post_count = g.db.execute('select count(post_id) from postit')
     cur_post_owner = g.db.execute("""select owner, count(post_id)
     from postit group by owner""")
@@ -224,7 +160,7 @@ def display_stats():
             'stupidity': stupidity
         })
     return render_template('statistics.html', stat_post_it=stat_post_count,
-                stat_post_owner=stat_post_owner, title=title)
+                stat_post_owner=stat_post_owner, title="Statistics")
 
 if __name__ == '__main__':
     app.run()
