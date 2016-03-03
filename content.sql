76fe734625def796dd86ba3a06243ed2b981d00e
Thibaut Moiroud <thibaut.moiroud@kozea.fr>
2016-03-03 11:52:13 +0100

    Modified .gitignore for credentials.

--- content.sql
+++
@@ -1,5 +0,0 @@
-insert into color (code_color, owner) values ('#ED6C09', 'Guillaume Ayoub');
-insert into color (code_color, owner) values ('#1F9ED5', 'Thibaut Moiroud');
-insert into color (code_color, owner) values ('#FD3F92', 'Clément Plasse');
-
-insert into postit (post_id, owner, text, date) values (1, 'Clément Plasse', '0 + 0 = tête à titi', '2015-12-05');
