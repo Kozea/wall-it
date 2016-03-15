dae4f19baa793f86a831b7ddeae032dd75369602
Thibaut Moiroud <thibaut.moiroud@kozea.fr>
2016-03-15 12:00:26 +0100

    Zoom with margin ok + fixed nav on top of the page.

--- static/js/home.js
+++ static/js/home.js
@@ -1,22 +1,21 @@
 $(document).ready(function() {
+    $('main').css('transform', 'scale(1)');
     var mainWidth = $('main').width();
     var mainHeight = $('main').height();
     $('article').draggable({
         cursor: 'move',
-        containment: 'main',
         scroll: true,
         scrollSensitivity: 20,
         scrollSpeed: 20,
         drag: function(event, ui){
             var matrix = $('main').css('transform');
             var values = matrix.match(/-?[\d\.]+/g);
-            if(values == null){
-                zoom = 1;
-            }
-            else{
-                var zoom = values[0];
-            }
-
+            // if(values == null){
+            //     zoom = 1;
+            // }
+            // else{
+            zoom = values[0];
+            // }
             ui.position.top = Math.min(Math.max(ui.position.top / zoom, 0), mainHeight - $(this).outerHeight());
             ui.position.left = Math.min(Math.max(ui.position.left / zoom, 0), mainWidth - $(this).outerWidth());
         }
@@ -32,41 +31,24 @@ $(document).ready(function() {
     });
 
     var scale = 1;
-    var xLast = 0;
-    var yLast = 0;
-    var xMain = 0;
-    var yMain = 0;
     $('main').bind('mousewheel', function(e) {
-        e.preventDefault();
-        var xScreen = e.pageX - $('main').offset().left;
-        var yScreen = e.pageY - $('main').offset().top;
 
-        xMain = xMain + ((xScreen - xLast) / scale);
-        yMain = yMain + ((yScreen - yLast) / scale);
+        e.preventDefault();
 
         if (e.originalEvent.wheelDelta >= 0)
         {
             if (scale < 1) {
-                scale += 0.03;
+                scale += 0.02;
             }
         }
         else
         {
             if (scale > 0.3) {
-                scale -= 0.03;
+                scale -= 0.02;
             }
         }
 
-        var xNew = (xScreen - xMain) / scale;
-        var yNew = (yScreen - yMain) / scale;
-
-        xLast = xScreen;
-        yLast = yScreen;
-
-        // $('main').css({
-        //     'transform-origin': xMain + 'px ' + yMain + 'px',
-        //     'transform': 'scale(' + scale + ')' + 'translate(' + xNew + 'px, ' + yNew + 'px' + ')'
-        // });
         $('main').css('transform', 'scale('+scale+')');
+        $('body').css('height', parseFloat($('main').css('height'))*scale + 'px');
     });
 });
